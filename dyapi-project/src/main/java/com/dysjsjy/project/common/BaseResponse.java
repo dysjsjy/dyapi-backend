2e2c61d3c39749c007f04c4e28afb58e20d1d772
dysjsjy <dysjsjy@163.com>
2024-12-13 22:27:01 +0800

    common && exception

---
+++ dyapi-project/src/main/java/com/dysjsjy/project/common/BaseResponse.java
@@ -0,0 +1,30 @@
+package com.dysjsjy.project.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class BaseResponse<T> implements Serializable {
+    private int code;
+    private T data;
+    private String message;
+
+    public BaseResponse(int code, T data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, T data) {
+        this.code = code;
+        this.data = data;
+        this.message = "";
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.data = null;
+        this.message = errorCode.getMessage();
+    }
+}
